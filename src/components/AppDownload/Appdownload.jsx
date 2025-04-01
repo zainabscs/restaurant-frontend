4804d607a1ff7130b34213791093c546956e9c60
Zaina234 <137766625+Zaina234@users.noreply.github.com>
2025-04-01 21:15:27 +0500

    Update Appdownload.jsx

--- src/components/AppDownload/Appdownload.jsx
+++ src/components/AppDownload/Appdownload.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Appdownload.css'
+import './AppDownload.css'
 import { assets } from '../../assets/frontend_assets/assets'
 const Appdownload = () => {
   return (
