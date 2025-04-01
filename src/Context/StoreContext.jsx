e84a1c46837e2fa3ceabf6cbbc23aaa24372fd20
Zaina Azam <zainaazam23@gmail.com>
2025-04-01 22:49:20 +0500

    new frontend changes

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
-    const url="http://localhost:4000"
+    const url="https://restaurant-backend-6iof.vercel.app/"
     const [token,setToken]=useState("");
     const [food_list,setFoodList]=useState([]);
     const addToCart = async (itemId) => {
