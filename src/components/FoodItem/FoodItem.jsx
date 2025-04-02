d8fea3e6516427f26f32d2c09651614f17f0a4c1
Zaina Azam <zainaazam23@gmail.com>
2025-04-02 13:20:23 +0500

    new day chnges

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -2,31 +2,33 @@ import React, { useContext } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/frontend_assets/assets'
 import { StoreContext } from '../../Context/StoreContext';
-const FoodItem = ({id,name,price,description,image}) => {
-  const{cartItems,addToCart,removeFromCart,url}=useContext(StoreContext)
+
+const FoodItem = ({ id, name, price, description, image }) => {
+  const { cartItems, addToCart, removeFromCart, url } = useContext(StoreContext);
+
   return (
     <div className='food-item'>
-        <div className="food-item-img-container">
-            <img className='food-item-img' src={url+'/images/'+image} alt="" />
-            {!cartItems[id]
-             ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="" />
-             : <div className='food-item-counter'>
-              <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+      <div className="food-item-img-container">
+        <img className='food-item-img' src={image} alt={name} /> {/* Use the full Cloudinary image URL */}
+        {!cartItems[id] 
+          ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt="" />
+          : <div className='food-item-counter'>
+              <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="" />
               <p>{cartItems[id]}</p>
-              <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
-             </div>
-            }
-        </div>
-        <div className="food-item-info">
-            <div className="food-item-name-rating">
-                <p>{name}</p>
-                <img src={assets.rating_starts} alt="" />
+              <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="" />
             </div>
-            <p className="food-item-description">{description}</p>
-            <p className='food-item-price'>${price}</p>
+        }
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
         </div>
+        <p className="food-item-description">{description}</p>
+        <p className='food-item-price'>${price}</p>
+      </div>
     </div>
   )
 }
 
-export default FoodItem
+export default FoodItem;
