ceba4805c226a10a17541e729982be15bfdf2c3e
zainabscs <zaina.bscs4707@iiu.edu.pk>
2025-04-25 20:59:11 +0500

    jwsjjhw

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,22 +1,34 @@
-import React, { useContext } from 'react'
-import './FoodDisplay.css'
-import { StoreContext } from '../../Context/StoreContext'
-import FoodItem from '../FoodItem/FoodItem'
-const FoodDisplay = ({ category }) => {
+import React, { useContext } from 'react';
+import './FoodDisplay.css';
+import { StoreContext } from '../../Context/StoreContext';
+import FoodItem from '../FoodItem/FoodItem';
 
-  const { food_list } = useContext(StoreContext)
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
+
   return (
     <div className='food-display' id='food-display'>
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-        {food_list.map((item, index) => {
+        {Array.isArray(food_list) && food_list.map((item, index) => {
           if (category === "All" || category === item.category) {
-            return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}></FoodItem>
+            return (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            );
+          } else {
+            return null;
           }
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodDisplay
+export default FoodDisplay;
